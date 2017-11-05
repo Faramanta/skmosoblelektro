7cf7e55e3d7841654f5db9840033f205a521d375
Faramanta <katuxa@yandex.ru>
2017-11-06 02:16:45 +0500

    меню

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,8 @@
 $(document).ready( function () {
-
+    $('.nav__link').on('click', function () {
+       $(this).addClass('active');
+       $(this).closest('.nav__item').find('.nav__sub-list').show();
+    });
 
 });
 
