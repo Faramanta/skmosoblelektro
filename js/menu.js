b4700bd55bfe3a70ee267b50d859d89bfe9be802
Faramanta <katuxa@yandex.ru>
2018-02-06 01:56:24 +0500

    fix

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
 $(document).ready( function () {
-    $('.nav__link').on('click', function () {
-       $(this).addClass('active');
-       $(this).closest('.nav__item').find('.nav__sub-list').show();
-    });
+    // $('.nav__link').on('click', function () {
+    //    $(this).addClass('active');
+    //    $(this).closest('.nav__item').find('.nav__sub-list').show();
+    // });
 
 });
 
